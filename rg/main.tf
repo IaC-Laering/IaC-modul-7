42555fba4554477b75b14cf326b7a541e6d80fa6
pauprska <przywuskap@gmail.com>
2025-11-01 13:57:41 +0100

    Update main.tf

--- rg/main.tf
+++ rg/main.tf
@@ -15,3 +15,19 @@ provider "azurerm" {
   }
   subscription_id = "7a3c6854-0fe1-42eb-b5b9-800af1e53d70" 
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.sa_name
+  resource_group_name      = var.rg_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags = {
+    environment = var.environment
+  }
+}
