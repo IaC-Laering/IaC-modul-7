6fcbf298b2eac8471bb0985dd5fab8c2a0d17189
pauprska <przywuskap@gmail.com>
2025-11-01 14:08:13 +0100

    Create variables.tf

---
+++ rg/variables.tf
@@ -0,0 +1,17 @@
+
+
+variable "rg_name" {
+  description = "Name of the resource group"
+}
+
+variable "sa_name" {
+  description = "Name of the storage account"
+}
+
+variable "location" {
+  description = "Azure region"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+}
